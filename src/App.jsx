7b13ecf725f96a0a80b431b339831f9736e0b190
mahdi <mahdi.95gm@gmail.com>
2025-09-03 17:38:24 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ const handleSubmit = async () => {
 
   return (
     <div style={{ maxWidth: '600px', margin: '40px auto', padding: '20px', fontFamily: 'sans-serif' }}>
-      <img src={logo} className= "logo" alt= "logo"/>
+      <img src={logo} className= 'logo' alt= 'logo'/>
       <h1 style={{ fontSize: '28px', color: '#4a5fa3', marginBottom: '20px', textAlign: 'center' }}>Daily Log</h1>
       <MentalSection data={formData} setData={setFormData} />
       <PhysicalSection data={formData} setData={setFormData} />
